deb72cc6e545c8da3d6b8a4a84f75e6395dc29e8
margaretanp <margaretanovia1911@gmail.com>
2025-03-17 21:57:03 +0700

    Jobsheet 4: ORM

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,14 +10,34 @@ class UserController extends Controller
 {
     public function index()
     {
-        //tambah data user dengan Eloquent Model
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data);//update data
+        UserModel::create($data); //tambah data user dengan Eloquent Model
+
+        $user = UserModel::all(); //ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_2',
+        //     'nama' => 'anager 2',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data); //tambah data user dengan Eloquent Model
+
+        // $user = UserModel::all(); //ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
+    //     //tambah data user dengan Eloquent Model
+    //     $data = [
+    //         'nama' => 'Pelanggan Pertama',
+    //     ];
+    //     UserModel::where('username', 'customer-1')->update($data);//update data
         
-       //coba akses model UserModel
-       $user = UserModel::all(); //ambil semua data dari tabel m_user   
-       return view('user', ['data' => $user]); 
+    //    //coba akses model UserModel
+    //    $user = UserModel::all(); //ambil semua data dari tabel m_user   
+    //    return view('user', ['data' => $user]); 
     }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -12,4 +12,11 @@ class UserModel extends Model
 
     protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan oleh model ini  
     protected $primaryKey = 'user_id'; //Mendefinisikan primary key dari tabel yang digunakan oleh model ini
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+    */
+    // protected $fillable = ['level_id', 'username', 'nama', 'password']; //Mendefinisikan kolom yang dapat diisi oleh model ini
+    protected $fillable = ['level_id', 'username', 'nama', ];
 }
